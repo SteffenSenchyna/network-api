c77324e66aa391e320d9fcf353790badacb3f719
SteffenSenchyna <steffen@cash2crypto.ca>
2023-03-09 11:05:56 -0800

    Set up /network/backup route
    Added connectivity to S3 and networking devices

--- api.py
+++ api.py
@@ -1,26 +1,12 @@
 import flask
 from flask import request, Response
-from app import interface, user
+from app import backup
 
 app = flask.Flask(__name__)
 
-
 @app.route("/network/backup", methods=["POST"])
-
-def get_int():
-    res = interface.get_int(request)
-    return res
-
-
-@app.route("/switch/interfaces", methods=["POST"])
-def create_int():
-    res = interface.create_int(request)
-    return res
-
-
-@app.route("/switch/users", methods=["POST"])
-def create_user():
-    res = user.create_user(request)
+def post_backup():
+    res = backup.postBackup(request)
     return res
 
 
--- app/backup.py
+++ app/backup.py
@@ -1,27 +1,55 @@
+import datetime
 import os
+import pytz
 import requests
 import json
 from flask import Response
 import napalm
 from dotenv import load_dotenv
+import boto3
 
-load_dotenv()
-
-def getBackup(ip):
-    driver = napalm.get_network_driver('ios')
-    device = driver(hostname=ip, username=os.getenv("USERNAME"), password=os.getenv("PASSWORD"), optional_args={'secret': os.getenv("SECRET")})
-    print(device)
+def putS3(hostname, config):
+    load_dotenv()
+    session = boto3.Session(
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+    aws_secret_access_key=os.environ["AWS_SECRET_KEY"]
+    )
+    utc_now = pytz.utc.localize(datetime.datetime.utcnow())
+    formated_date = utc_now.strftime("%Y-%m-%dT%H-%M-%S")
+    s3_client = session.resource("s3")
+    s3_object = s3_client.Object("network-conf", f'{hostname}/{formated_date}.txt')
     try:
-        device.open()
-        config = device.get_config()
-        device.close()
-        return config['running']
-
+        result = s3_object.put(Body=(config))
+        return result
     except Exception as e:
-        print(f"Error: {e}")
-        return None
+        return e
+
+    
+    
+
+def postBackup(request):
+    print(request)
+    ips = request.get_json()["ips"]
+    print(ips)
+    load_dotenv()
+    driver = napalm.get_network_driver('ios')
+    for i in ips:
+        device = driver(hostname=i, username=os.environ["USER_NAME"], password=os.environ["PASSWORD"], optional_args={'secret': os.environ["SECRET"]})
+        try:
+            device.open()
+            hostname = device.get_facts()['hostname']
+            config = device.get_config()['running']
+            device.close()
+            putS3(hostname, config)
+        except Exception as e:
+            print(f"Error: {e}")
+            return Response(f"Error: {e}", status=400)
+        
+    return Response("Succesfully posted all backups to S3", status=201)
 
 
-getBackup("10.0.5.21")
+
+
+
 
 
